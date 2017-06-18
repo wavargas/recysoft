aee98882c55dea56e75b48432e6f2ff4cc5008c7
William Vargas <William Vargas@wvargas>
2017-06-17 20:41:40 -0500

    Cambios con autoload y namespace

--- config/config.php
+++ config/config.php
@@ -1,24 +1,8 @@
-<?php
-spl_autoload_register();
+<?php namespace config;
+include_once 'config_clases.php';
 #include('..\datos/CData.php');
 define ('SERVIDOR', 'localhost');
 define ('USUARIOBD', 'root');
 define ('CONTRASENIABD', '');
 define ('NOMBREBD', 'RecySoft');
-
-$db = new datos\clases\CData();
-$db->Conectar();
-$query='select * from usuarios';
-$resultado = $db->EjecutarConsulta($query);
-echo '<pre>', print_r($resultado), '</pre>';
-echo 'Resultados :','<br>';
-if($resultado)
-{  while($fila=$db->RecuperarResultados($resultado))
-	{
-	#echo '<pre>', $fila['nombres'], " ", $fila['apellidos'], '</pre>';
-	echo '<pre>', print_r($fila), '</pre>';
-	}
-}
-$db->LiberarResultado($resultado);
-$db->CerrarConexion();
 ?>
